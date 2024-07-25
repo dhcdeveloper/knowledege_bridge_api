80d039b7755c7df334cbdf8b3776b72b499f850c
MrLv <boqian.lv@dhc.com.cn>
2024-07-25 13:59:38 +0800

    zsq_deleted_at 改修

--- lang/ja/message.php
+++ lang/ja/message.php
@@ -5,6 +5,13 @@ return [
         'user' => [
             'wrong_password' => 'ログインに失敗しました。パスワードが間違っています',
             'wrong_email' => 'ログインに失敗しました。メールアドレスが存在しません',
+            'same_email' => 'メールボックスは既に使用中です',
         ],
     ],
+    'success' => [
+        'user' => [
+            'success_register' => '登録に成功しました',
+        ],
+    ],
+
 ];
